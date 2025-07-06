f06c26bb8dad87fdee3f7a8733333dc80758724b
Aditya Salian <adityasalian865@gmail.com>
2025-07-06 12:36:29 +0000

    setting changes

--- loan_approval/loan_approval/settings.py
+++ loan_approval/loan_approval/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
-django_heroku.settings(locals())
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
+# django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
